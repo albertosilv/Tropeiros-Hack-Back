d61aa2d6728269ece874957705b8cfeeb3f6291f
albertosilv <jose.alberto.silva@ccc.ufcg.edu.br>
2021-06-11 02:59:14 -0300

    fixed 2 filter

--- src/Routers/AccidentsRouter.js
+++ src/Routers/AccidentsRouter.js
@@ -6,8 +6,4 @@ Router.get('/:id', authentication, AccidentsController.show);
 Router.post('/', authentication, AccidentsController.store);
 Router.put('/:id', authentication, AccidentsController.update);
 Router.delete('/:id', authentication, AccidentsController.destroy);
-<<<<<<< HEAD
-=======
-Router.get('/filter',authentication,FilteringController.filter)
->>>>>>> cabff5bc59135120290cab4d35e4d9e61f946386
 module.exports = Router;
