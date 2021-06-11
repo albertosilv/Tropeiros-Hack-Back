a330884865f5b69fc5dbc5c3843b6b600cbd0c35
albertosilv <jose.alberto.silva@ccc.ufcg.edu.br>
2021-06-11 04:03:14 -0300

    xlsx

--- src/Repositories/xlsx.js
+++ src/Repositories/xlsx.js
@@ -1,24 +1,9 @@
 const xls = require('node-xlsx')
-const planilha2019 = xls.parse('../xls/2019.xlsx')
-const planilha2020 = xls.parse('../xls/2020.xlsx')
-nova2020 = planilha2020[0].data.map(e=>{
-    return e[17]
-})
-nova2020 = nova2020.filter(function(el, i) {
-    return nova2020.indexOf(el) === i;
-});
+const pla = require('./xlsx2')()
+const Accidents = require('../Models/AccidentsModel')
+create = async ()=>{
+    const data = await Accidents.collection.insert(pla)
+    console.log(data)
+}
+create()
 
-nova2019 = planilha2019[0].data.map(e=>{
-    return e['7']
-})
-nova2019 = nova2019.filter(function(el, i) {
-    return nova2019.indexOf(el) === i;
-});
-let dados = nova2020.concat(nova2019)
-
-dados=dados.filter(function(el, i) {
-    return dados.indexOf(el) === i;
-});
-dados.forEach((element,index) => {
-    console.log(`${index}:'${element}',`)
-});
--- src/Repositories/xlsx2.js
+++ src/Repositories/xlsx2.js
@@ -1,31 +1,30 @@
 const xls = require('node-xlsx')
 const planilha2019 = xls.parse('../xls/2019.xlsx')
 const planilha2020 = xls.parse('../xls/2020.xlsx')
-const data = require('./data')
 module.exports =()=> {
     nova2020 = planilha2020[0].data.map(e=>{
         return {
-            sequ:e[1],
+            id:e[1],
             origem:e[2],
             tipo:e[16],
             rua:e[3],
-            bairro:[10],
-            data:data(e[13],e[12],2020,e[11]),
+            bairro:e[10],
+            data:new Date(e[13],e[12],2020,e[11]),
             cruzamento:e[8],
-            semafora:e[9],
+            semaforo:e[9],
             observacao:e[77]
         }
     })
     nova2019 = planilha2019[0].data.map(e=>{
         return {
-            sequ:e[1],
+            id:e[1],
             origem:e[2],
             tipo:e[16],
             rua:e[3],
-            bairro:[10],
-            data:data(e[13],e[12],2020,e[11]),
+            bairro:e[10],
+            data:new Date(e[13],e[12],2020,e[11]),
             cruzamento:e[8],
-            semafora:e[9],
+            semaforo:e[9],
             observacao:e[77]
         }
     })
