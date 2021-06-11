b079786920a8790dee9104f8ff5870c1fd86cd35
Vinícius Lins <viniciusaclins@gmail.com>
2021-06-11 01:55:45 -0300

    Bug fix

--- src/Routers/AccidentsRouter.js
+++ src/Routers/AccidentsRouter.js
@@ -7,5 +7,5 @@ Router.get('/:id', authentication, AccidentsController.show);
 Router.post('/', authentication, AccidentsController.store);
 Router.put('/:id', authentication, AccidentsController.update);
 Router.delete('/:id', authentication, AccidentsController.destroy);
-Router.get('/filter',authentication,FilteringController)
+Router.get('/filter',authentication,FilteringController.filter)
 module.exports = Router;
