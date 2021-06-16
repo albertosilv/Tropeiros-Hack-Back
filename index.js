81ab9490ad8fac91e7e65fd951ad1e6cb12756a8
Vinícius Lins <viniciusaclins@gmail.com>
2021-06-15 21:31:17 -0300

    CORS moved

--- index.js
+++ index.js
@@ -12,15 +12,17 @@ app.use(express.json());
 app.use(express.urlencoded({extended:true}))
 app.use(morgan('dev'))
 
-app.use('/user',user);
-app.use('/accidents',accidents)
-app.use('/filter',filtering)
 app.use(function (req, res, next) {
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Connection, authorization');
     res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
     next();
 });
+
+app.use('/user',user);
+app.use('/accidents',accidents)
+app.use('/filter',filtering)
+
 
 const port = process.env.PORT || 4000
 
