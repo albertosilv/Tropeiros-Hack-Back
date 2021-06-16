db89e77952dffd359e3e70bf4d5acb1f7646b1b6
Vinícius Lins <viniciusaclins@gmail.com>
2021-06-15 23:34:30 -0300

    Fix bugs in LoginController

--- src/Controllers/AccidentsController.js
+++ src/Controllers/AccidentsController.js
@@ -70,8 +70,7 @@ class Accident{
     static  async index(req,res){
         try{
             const {pag} = req.body
-            const accidents = await AccidentsModel.find()
-            console.log(accidents.length)
+            const accidents = await AccidentsModel.find().skip((pag-1)*7).limit(7);
             if(accidents.length == 0) return res.status(400).json({error: "Não há acidentes registrados"})
  
             return res.status(200).json(accidents);
