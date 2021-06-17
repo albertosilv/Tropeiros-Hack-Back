eb0d24d6b17640a0e009460e876471c165547a09
albertosilv <jose.alberto.silva@ccc.ufcg.edu.br>
2021-06-17 02:43:44 -0300

    auth

--- src/Routers/GraphicRouter.js
+++ src/Routers/GraphicRouter.js
@@ -1,7 +1,7 @@
 const Router = require('express').Router();
 const GraphicController = require('../Controllers/GraphicController');
 const authentication =  require('../middleware/auth');
-Router.get('/bairros', GraphicController.bairros);
-Router.get('/tipos', GraphicController.tipos);
-Router.get('/meses',GraphicController.meses)
+Router.get('/bairros', authentication, GraphicController.bairros);
+Router.get('/tipos', authentication,GraphicController.tipos);
+Router.get('/meses',authentication,GraphicController.meses)
 module.exports = Router;
